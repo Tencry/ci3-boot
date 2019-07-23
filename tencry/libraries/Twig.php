41375fd992a85c146555ca5c2094ca8731e0c477
Arman [Tencry] Sarsenov <tencry@mail.ru>
2019-07-23 17:27:29 +0600

    Twig lib configuration.

--- tencry/libraries/Twig.php
+++ tencry/libraries/Twig.php
@@ -6,23 +6,25 @@ class Twig {
 	private $_loader;
 	private $_twig;
 
+	protected $EXT = 'twig';
+
 	public function __construct()
 	{
 		$this->_loader = new \Twig\Loader\FilesystemLoader(VIEWPATH);
 		$this->_twig   = new \Twig\Environment($this->_loader, [
 			'debug' => ENVIRONMENT !== 'production',
-		    'cache' => ENVIRONMENT === 'production' ? APPPATH . 'cache' : false,
+		    'cache' => ENVIRONMENT !== 'development' ? APPPATH . 'cache' : false,
 		    'strict_variables' => ENVIRONMENT === 'testing' ? true : false,
 		]);
 	}
 
 	public function load($file)
 	{
-		return $this->_twig->load($file);
+		return $this->_twig->load($file . '.' . $this->EXT);
 	}
 
 	public function render($file, $data = [])
 	{
-		return $this->_twig->render($file, $data);		
+		return $this->_twig->render($file . '.' . $this->EXT, $data);
 	}
 }
